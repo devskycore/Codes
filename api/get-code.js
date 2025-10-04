daf094a7fdd67302fd4b29cab0704a64d428a322
SkyCore <devskycore@gmail.com>
2025-10-04 09:00:19 -0300

    Create get-code.js

---
+++ api/get-code.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  const token = req.query.token;
+  if (token !== process.env.ACCESS_TOKEN) {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+  res.status(200).json({ code: process.env.SECRET_CODE });
+}
